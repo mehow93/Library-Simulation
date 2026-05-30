25e76ceab5d94763222c61828076845d563d3a68
mehow93 <michmad93@gmail.com>
2026-05-30 14:19:26 +0200

    CreateBookTest added and it passes

--- Library/Library/tests/tests.cpp
+++ Library/Library/tests/tests.cpp
@@ -1,9 +1,18 @@
 #include <gtest/gtest.h>
 #include "../Librarian.hpp"
 
-TEST(CalculatorTest, AddPositiveNumbers)
+TEST(LibrarianTest, CreateBookTest)
 {
+    Librarian testLibrarian;
+    Book modelBook;
+    modelBook.authorsSurname = "authorsSurname";
+    modelBook.authorsName = "authorsName";
+    modelBook.title = "Title";
 
+    auto testBook = testLibrarian.createBookRecord("authorsSurname", "authorsName", "Title");
 
-    EXPECT_EQ(5, 5);
+    ASSERT_FALSE(testBook.isBorrowed);
+    ASSERT_STREQ(modelBook.authorsSurname.c_str(), testBook.authorsSurname.c_str());
+    ASSERT_STREQ(modelBook.authorsName.c_str(), testBook.authorsName.c_str());
+    ASSERT_STREQ(modelBook.title.c_str(), testBook.title.c_str());
 }
