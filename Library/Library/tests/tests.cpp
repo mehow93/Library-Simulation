681c812d0f9ee17165f62a92b08b858b27e853df
mehow93 <michmad93@gmail.com>
2026-09-05 18:17:31 +0200

    test fixed

--- Library/Library/tests/tests.cpp
+++ Library/Library/tests/tests.cpp
@@ -40,23 +40,33 @@ TEST_F(LibrarianTest, IDshouldBe2WhenTwoLibrariansWereCreated)
     ASSERT_EQ(2, testLibrarian.getID());
 }
 
-TEST_F(LibrarianTest, 2LibrariansInOneTestLastOneWithIDEq3)
+TEST_F(LibrarianTest, 2LibrariansInOneTestLastOneWithIDEq3)//przepisz ten test
 {
     Librarian testLibrarian2("Jan", "Naj");
-    ASSERT_EQ(testLibrarian2.getID(), 2);
-
     Librarian testLibrarian3("Ola", "Cola");
+
+    ASSERT_EQ(testLibrarian2.getID(), 2);
     EXPECT_EQ(testLibrarian3.getID(), 3);
 
 }
 
-TEST_F(LibrarianTest, CreateBookTest)
+TEST_F(LibrarianTest, createBookTest)
 {
-    Book modelBook = {"Title", "authorsName", "authorsSurname" };
-    auto testBook = m_testLibrarian.createBookRecord("authorsSurname", "authorsName", "Title");
+    Book modelBook = {"Title", "authorsSurname", "authorsName"};
+    auto testBook = m_testLibrarian.createBookRecord("Title", "authorsSurname", "authorsName");
 
     ASSERT_FALSE(testBook.isBorrowed);
     ASSERT_STREQ(modelBook.authorsSurname.c_str(), testBook.authorsSurname.c_str());
     ASSERT_STREQ(modelBook.authorsName.c_str(), testBook.authorsName.c_str());
     ASSERT_STREQ(modelBook.title.c_str(), testBook.title.c_str());
 }
+
+/*TEST_F(LibrarianTest, addBookTest_EmptyLibraryBooks)//consider mocking Library class and its function getLibraryBooks()
+{
+    std::vector<std::vector<Book>> testBooksVector;
+
+    ASSERT_EQ(1, testBooksVector.size());
+    EXPECT_EQ(1, testBooksVector[0].size());
+    m_testLibrarian.addBook("testTitle", "testSurname", "testName", testBooksVector);
+
+}*/
